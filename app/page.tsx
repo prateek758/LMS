505c94e4fcd78579b398918cf0e1f632e9755136
Prateekrana20 <146994117+Prateekrana20@users.noreply.github.com>
2025-05-27 23:56:45 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ const Page = () => {
       <h1 className='text-2xl underline'>
         Welcome to my SaaS app
       </h1>
-      <a href="sign-in">
-      <Button className='cursor-pointer m-2'>Sign In</Button>
-      </a>
     </>
   )
 }

8fd56d21f4c2f289cd0314e388056356ad19864d
prateek758 <prateekrana758@gmail.com>
2025-06-01 14:31:43 +0530

    library setup

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -23,6 +23,18 @@ export const getAllCompanions = async ({limit=10, page=1, subject, topic} : GetA
 
     if(subject && topic){
         query = query.ilike('subject', `%${subject}%`)
-        .or(`topic.ilike`)
+        .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+    } else if(subject){
+        query = query.ilike('subject', `%${subject}%`)
+    } else if(topic){
+        query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
     }
+
+    query = query.range((page - 1) * limit, page * limit - 1);
+
+    const {data: companions, error} = await query;
+    
+    if(error) throw new Error(error.message);
+
+    return companions;
 }
